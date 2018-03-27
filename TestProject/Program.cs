acccf9adbd44357e5ddc2ac82d78c7c4718664b0
SomeTestUser129 <office@todvachev.com>
2018-03-28 02:36:19 +0300

    vxcvxc vxcxcv cvxcvxcv xcvv

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -21,6 +21,8 @@ namespace TestProject
             Console.WriteLine(123);
             Console.WriteLine(123);
             Console.WriteLine(123);
+            Console.WriteLine(123);
+            Console.WriteLine(123);
         }
     }
 }
